9f5872bc40a72b448a8afda6e345a991301d5c6b
SzDave <szantodavid.mail@gmail.com>
2024-06-16 17:31:00 +0200

    Initial commit.

---
+++ gitter.bat
@@ -0,0 +1,5 @@
+@REM Gitter Version 1.0.0
+echo "Gitter V.1.0.0"
+git add .
+git commit -m %1
+git push origin %2
---
+++ src/main/java/com/beaver/softw/run/Main.java
@@ -0,0 +1,7 @@
+package com.beaver.softw.run;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello, Beaver!!44!!");
+	}
+}
