2d56309d9e805fb651bcbe6665a5b7e5cd5f8669
David Szanto <szantodavid.mail@gmail.com>
2024-06-20 22:48:52 +0200

    Test

--- gitter2.bat
+++ gitter2.bat
@@ -1,6 +1,16 @@
+@REM Gitter Version 1.0.0
 @echo off
+echo "Gitter V.1.0.0"
 cls
-SET v1=%1
-if %v1%=="Up" (
-    echo "Pushing"
+SET mode=%1
+
+if %mode%==--up (
+    echo Pushing...
+    git add .
+    git commit -m %2
+    git push origin %3
+)
+
+if %mode%==dwn (
+
 )
