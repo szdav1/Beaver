2efb577c82f7240e2d7479640610e596f55451e3
SzDave <szantodavid.mail@gmail.com>
2024-06-26 23:24:11 +0200

    Update gitter.

--- gitter.bat
+++ gitter.bat
@@ -1,8 +1,10 @@
 @REM Gitter Version 1.5.0
 @echo off
 cls
-echo "Gitter V.1.0.0"
+echo Gitter V.1.5.0
 SET mode=%1
+
+if not defined mode echo Mode not specified. Use --up or --dwn.
 
 if %mode%==--up (
     echo Pushing...
