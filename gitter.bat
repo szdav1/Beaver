160d6db2e7142225468f4821b461bb5aae21adec
SzDave <szantodavid.mail@gmail.com>
2024-07-01 22:26:20 +0200

    Committing before pulling.

--- gitter.bat
+++ gitter.bat
@@ -1,8 +1,8 @@
 @REM Gitter Version 1.5.0
 @echo off
 cls
-echo Gitter V.2.0.1
-echo ==============
+echo  Gitter V.2.0.1
+echo ================
 SET mode=%1
 
 if not defined mode goto mnsel
@@ -13,6 +13,10 @@ if %mode%==--dwn goto pulll
 
 if %mode%==--dwn-ovr goto pullhardl
 
+if %mode%==? goto helpl
+
+if %mode%==help goto helpl
+
 :pushl
 SET msg=%2
 SET branch=%3
@@ -21,7 +25,7 @@ if not defined msg goto msgndel
 
 if not defined branch goto branchndel
 
-echo >> Pushing to %branch%
+echo Pushing to %branch%
 
 git add .
 git commit -m %msg%
@@ -33,7 +37,7 @@ SET branch=%2
 
 if not defined branch goto branchndel
 
-echo << Pulling from %branch%...
+echo Pulling from %branch%...
 
 git add .
 git commit -m "Committing before pulling."
@@ -45,10 +49,39 @@ SET branch=%2
 
 if not defined branch goto branchndel
 
-echo << Overriding from %branch%...
+echo Overriding from %branch%...
 
 git fetch --all
 git reset --hard origin/%branch%
+goto exitl
+
+:helpl
+echo  Helper
+echo ========
+echo As first argument, define the mode:
+echo --up: For uploading, pushing to a repository
+echo --dwn: For downloading, pulling from a repository
+echo --dwn-ovr: For downloading and overriding, fetching and hard resetting from a repository
+echo ==========================================================================================
+echo  Pushing
+echo =========
+echo When pushing, the second argument should be the commit message
+echo The third argument in case of pushing should be the name of the branch where the push should be done
+echo ====================================================================================================
+echo  Pulling
+echo =========
+echo When pulling, the second argument should be the name of the branch from where the pull should be conducted
+echo ==========================================================================================================
+echo  Hard Pulling
+echo ==============
+echo When hard-pulling, the second argument should be the name of the branch from where the hard-pull should be conducted
+echo ====================================================================================================================
+echo  Examples
+echo ==========
+echo gitter --up "This is a commit message" branch
+echo gitter --dwn branch
+echo gitter --dwn-ovr branch
+
 goto exitl
 
 :msgndel
